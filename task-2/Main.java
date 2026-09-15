81cb5eabb008466b20abb3de99ae273089dcee65
klrl007 <kirillmaltcev07@gmail.com>
2026-09-15 11:16:47 -0700

    feat: задача 2 - справочник команд java

---
+++ task-2/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        switch (command) {
+            case "System.out.println()":
+                System.out.println("Это команда вывода на печать");
+                break;
+            case "if":
+                System.out.println("Это условный оператор");
+                break;
+            case "else":
+                System.out.println("Это альтернативная конструкция условного оператора");
+                break;
+            default:
+                System.out.println("Раздел в разработке");
+        }       
+    }
+}

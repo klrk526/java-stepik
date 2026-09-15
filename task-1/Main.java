4cc58cd17b24a09ef6ebd47b7437d72ca08db520
klrl007 <kirillmaltcev07@gmail.com>
2026-09-14 23:12:14 -0700

    fix:исправлена опечатка в выводе

--- task-1/Main.java
+++ task-1/Main.java
@@ -8,21 +8,42 @@ class Main {
         int x1 = Integer.parseInt(nums[0]);
         int x2 = Integer.parseInt(nums[1]);
         int x3 = Integer.parseInt(nums[2]);
-        if (x1 % 2 == 0) {
+        if (x1 % 2 == 0 && x2 % 2 == 0 && x3 % 2 == 0) {
             if (x1 > x2 && x1 > x3) {
+                System.out.println(x1);
+            } else if (x2 > x1 && x2 > x3) {
+                System.out.println(x2);
+            } else {
                 System.out.println(x3);
             }
-        } else if (x2 % 2 == 0) {
-            if (x2 > x1 && x2 > x3) {
+        } else if (x1 % 2 == 0 && x2 % 2 == 0 && x3 % 2 != 0) {
+            if (x1 > x2) {
+                System.out.println(x1);
+            } else {
                 System.out.println(x2);
             }
-        } else if (x3 % 2 == 0) {
-            if (x3 > x1 && x3 > x2) {
+        } else if (x1 % 2 == 0 && x3 % 2 == 0 && x2 % 2 != 0) {
+            if (x1 > x3) {
+                System.out.println(x1);
+            } else {
                 System.out.println(x3);
             }
-        } else {
-            System.out.println("Четных чисел нет");
+        } else if (x2 % 2 == 0 && x3 % 2 == 0 && x1 % 2 != 0) {
+            if (x2 > x3) {
+                System.out.println(x2);
+            } else {
+                System.out.println(x3);
+            }
+        } else if (x1 % 2 == 0 && x2 % 2 != 0 && x3 % 2 != 0) {
+            System.out.println(x1);
+        } else if (x1 % 2 != 0 && x2 % 2 == 0 && x3 % 2 != 0) {
+            System.out.println(x2);
+        } else if (x1 % 2 != 0 && x2 % 2 != 0 && x3 % 2 ==0) {
+            System.out.println(x3);
+        }  
+        else {
+            System.out.println("Чётных чисел нет");
+        }
+        
         }
     }
-}
-

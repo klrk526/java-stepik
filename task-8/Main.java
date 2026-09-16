957d8ff92200efa883bcb97ea271f0e515a61022
klrl007 <kirillmaltcev07@gmail.com>
2026-09-16 10:36:06 -0700

    задание 8 - исправил ошибку

--- task-8/Main.java
+++ task-8/Main.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        long num = sc.nextLong();
         while (num != 0) {
-            int num1 = num % 10;
-            System.out.println(num1);
-            int num2 = num / 10;
+            System.out.print(num % 10);
+            num = num / 10;
         }
     }
 }
